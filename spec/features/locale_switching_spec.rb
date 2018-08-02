b244906ad15f00098fe0ffdf0c69120338ad6ab5
Alexander Khlipun <alex.khlipun@icloud.com>
2018-08-03 00:29:39 +0300

    Add locale switching test

---
+++ spec/features/locale_switching_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'locale switching' do
+  context 'when the requested locale present' do
+    scenario 'locale switching from English to Russian' do
+      visit root_url
+      click_link "RU"
+      
+      expect(current_url).to eq(root_url(locale: 'ru'))
+    end
+  end
+  
+  context 'when the requested locale does not present' do
+    scenario 'locale switching from English to Portuguese' do
+      visit root_url(locale: 'pt')
+      
+      expect(current_url).to eq(root_url)
+    end
+  end
+end
